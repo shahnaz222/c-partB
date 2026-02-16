838327bd70a1e1742870d2d472b54afb95ae4c6c
shahnaz222 <shahnaz12388@gmail.com>
2026-02-16 15:24:12 +0530

    Add files via upload

---
+++ qsn1.cpp
@@ -0,0 +1,30 @@
+
+
+
+    //Write a program to swap two numbers using a third variable
+
+    #include<iostream>
+    using namespace std;
+
+    int main(){
+
+       int a,b,c;
+       cout<<"enter 1st number : ";
+       cin>>a;
+
+       cout<<"enter 2nd number : ";
+       cin>>b;
+
+       //swap two numbers in 3rd variable
+
+      c=a;
+      a=b;
+      b=c;
+
+      cout<<"after swapping : "<<a <<" "<<b;
+      return 0;
+
+
+
+
+    }
